7dc7b2326762a0a80761483cb44e28df29128d66
Felix Dangel <fdangel@tue.mpg.de>
2022-12-13 17:03:09 +0100

    [ADD] `einconv2d` (equivalent of `torch.nn.functional.conv2d`)

--- test/cases.py
+++ test/cases.py
@@ -61,3 +61,58 @@ CONV_1D_FUNCTIONAL_CASES = [
     },
 ]
 CONV_1D_FUNCTIONAL_IDS = [make_id(case) for case in CONV_1D_FUNCTIONAL_CASES]
+
+CONV_2D_FUNCTIONAL_CASES = [
+    # no kwargs
+    {
+        "seed": 0,
+        # (batch_size, in_channels, num_pixels_h, num_pixels_w)
+        "input_fn": lambda: rand(2, 3, 50, 40),
+        # (out_channels, in_channels // groups, kernel_size_h, kernel_size_w)
+        "weight_fn": lambda: rand(4, 3, 5, 5),
+        "bias_fn": lambda: None,
+        # stride, padding, dilation, groups
+        "conv_kwargs": {},
+    },
+    # non-default stride, bias
+    {
+        "seed": 0,
+        "input_fn": lambda: rand(2, 3, 50, 40),
+        "weight_fn": lambda: rand(4, 3, 5, 5),
+        "bias_fn": lambda: rand(4),
+        "conv_kwargs": {"stride": 2},
+    },
+    # non-default stride, bias, groups
+    {
+        "seed": 0,
+        "input_fn": lambda: rand(2, 4, 50, 40),
+        "weight_fn": lambda: rand(6, 2, 5, 5),
+        "bias_fn": lambda: rand(6),
+        "conv_kwargs": {"stride": 3, "groups": 2},
+    },
+    # non-default bias, padding
+    {
+        "seed": 0,
+        "input_fn": lambda: rand(2, 3, 50, 40),
+        "weight_fn": lambda: rand(4, 3, 5, 6),
+        "bias_fn": lambda: rand(4),
+        "conv_kwargs": {"padding": 2},
+    },
+    # padding, stride, dilation specified as tuple
+    {
+        "seed": 0,
+        "input_fn": lambda: rand(2, 3, 50, 40),
+        "weight_fn": lambda: rand(4, 3, 5, 3),
+        "bias_fn": lambda: rand(4),
+        "conv_kwargs": {"padding": (2, 1), "stride": (3, 2), "dilation": (1, 2)},
+    },
+    # non-default dilation
+    {
+        "seed": 0,
+        "input_fn": lambda: rand(2, 3, 50, 40),
+        "weight_fn": lambda: rand(4, 3, 5, 6),
+        "bias_fn": lambda: None,
+        "conv_kwargs": {"dilation": 2},
+    },
+]
+CONV_2D_FUNCTIONAL_IDS = [make_id(case) for case in CONV_2D_FUNCTIONAL_CASES]
