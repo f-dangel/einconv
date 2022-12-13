c1913cbf101f5d1177ea4b2a1215cccc5b73e09f
Felix Dangel <fdangel@tue.mpg.de>
2022-12-13 19:17:22 +0100

    [REF] Replace `einconv{1,2,3}d` by `einconvNd` with arbitrary `N`

---
+++ einconv/utils.py
@@ -0,0 +1,41 @@
+"""Utility functions for ``einconv``."""
+
+from typing import Tuple, Union
+
+
+def _tuple(conv_hyperparameter: Union[int, Tuple[int, ...]], N: int) -> Tuple[int, ...]:
+    """Convert a convolution hyperparameter specified as int or tuple to tuple format.
+
+    This function is a generalization of
+    ``torch.nn.modules.utils.{_single, _pair, _triple}``.
+
+    Args:
+        conv_hyperparameter: kernel size, padding, dilation, or stride, accepted as
+            input by PyTorch's convolution functionals or modules.
+
+    Returns:
+        A tuple with ``N`` entries containing the hyperparameter to be used for each
+        dimension.
+
+    Raises:
+        ValueError: If the hyperparameter is not specified correctly.
+    """
+    if isinstance(conv_hyperparameter, int):
+        return N * (conv_hyperparameter,)
+
+    if not isinstance(conv_hyperparameter, tuple):
+        raise ValueError(
+            "Convolution hyperparameters must be specified as int or tuple of int."
+            + f" Got {conv_hyperparameter}."
+        )
+    elif len(conv_hyperparameter) != N:
+        raise ValueError(
+            f"Convolution hyperparameters must be specified as tuple of length {N}."
+            + f" Got tuple of length {len(conv_hyperparameter)}."
+        )
+    elif any(not isinstance(h, int) for h in conv_hyperparameter):
+        raise ValueError(
+            f"All hyperparameters must be integers. Got {conv_hyperparameter}."
+        )
+    else:
+        return conv_hyperparameter
--- test/test_einconvnd.py
+++ test/test_einconvnd.py
@@ -17,13 +17,13 @@ from pytest import mark
 from torch import device, manual_seed
 from torch.nn.functional import conv1d, conv2d, conv3d
 
-from einconv.einconvnd import einconv1d, einconv2d, einconv3d
+from einconv.einconvnd import einconvNd
 
 
 @mark.parametrize("device", DEVICES, ids=DEVICE_IDS)
 @mark.parametrize("case", CONV_1D_FUNCTIONAL_CASES, ids=CONV_1D_FUNCTIONAL_IDS)
 def test_einconv1d(case: Dict, device: device):
-    """Compare PyTorch's conv1d with einconv's einconv1d.
+    """Compare PyTorch's conv1d with einconv's einconvNd.
 
     Args:
         case: Dictionary describing the test case.
@@ -37,7 +37,7 @@ def test_einconv1d(case: Dict, device: device):
         bias = bias.to(device)
 
     conv_output = conv1d(x, weight, bias=bias, **case["conv_kwargs"])
-    einconv_output = einconv1d(x, weight, bias=bias, **case["conv_kwargs"])
+    einconv_output = einconvNd(x, weight, bias=bias, **case["conv_kwargs"])
 
     report_nonclose(conv_output, einconv_output)
 
@@ -45,7 +45,7 @@ def test_einconv1d(case: Dict, device: device):
 @mark.parametrize("device", DEVICES, ids=DEVICE_IDS)
 @mark.parametrize("case", CONV_2D_FUNCTIONAL_CASES, ids=CONV_2D_FUNCTIONAL_IDS)
 def test_einconv2d(case: Dict, device: device):
-    """Compare PyTorch's conv2d with einconv's einconv2d.
+    """Compare PyTorch's conv2d with einconv's einconvNd.
 
     Args:
         case: Dictionary describing the test case.
@@ -59,7 +59,7 @@ def test_einconv2d(case: Dict, device: device):
         bias = bias.to(device)
 
     conv_output = conv2d(x, weight, bias=bias, **case["conv_kwargs"])
-    einconv_output = einconv2d(x, weight, bias=bias, **case["conv_kwargs"])
+    einconv_output = einconvNd(x, weight, bias=bias, **case["conv_kwargs"])
 
     report_nonclose(conv_output, einconv_output)
 
@@ -67,7 +67,7 @@ def test_einconv2d(case: Dict, device: device):
 @mark.parametrize("device", DEVICES, ids=DEVICE_IDS)
 @mark.parametrize("case", CONV_3D_FUNCTIONAL_CASES, ids=CONV_3D_FUNCTIONAL_IDS)
 def test_einconv3d(case: Dict, device: device):
-    """Compare PyTorch's conv3d with einconv's einconv3d.
+    """Compare PyTorch's conv3d with einconv's einconvNd.
 
     Args:
         case: Dictionary describing the test case.
@@ -81,6 +81,6 @@ def test_einconv3d(case: Dict, device: device):
         bias = bias.to(device)
 
     conv_output = conv3d(x, weight, bias=bias, **case["conv_kwargs"])
-    einconv_output = einconv3d(x, weight, bias=bias, **case["conv_kwargs"])
+    einconv_output = einconvNd(x, weight, bias=bias, **case["conv_kwargs"])
 
     report_nonclose(conv_output, einconv_output)
