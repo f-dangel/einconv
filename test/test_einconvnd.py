1cd23e8a93f3ee090d4c4b00f1faf3759d931e95
Felix Dangel <48687646+f-dangel@users.noreply.github.com>
2022-12-13 17:34:51 +0100

    [ADD] `einconv3d` functional (equivalent of `torch.nn.functional.conv3d`) (#4)
    * [FIX] Correct padding check for `einconv2d`
    
    * [ADD] `einconv3d` functional (equivalent of `torch.nn.functional.conv3d`)
    
    * [DOC] Fix typo
    
    * [REF] Improve type annotation (tuple entries)

--- test/test_einconvnd.py
+++ test/test_einconvnd.py
@@ -5,6 +5,8 @@ from test.cases import (
     CONV_1D_FUNCTIONAL_IDS,
     CONV_2D_FUNCTIONAL_CASES,
     CONV_2D_FUNCTIONAL_IDS,
+    CONV_3D_FUNCTIONAL_CASES,
+    CONV_3D_FUNCTIONAL_IDS,
     DEVICE_IDS,
     DEVICES,
 )
@@ -13,9 +15,9 @@ from typing import Dict
 
 from pytest import mark
 from torch import device, manual_seed
-from torch.nn.functional import conv1d, conv2d
+from torch.nn.functional import conv1d, conv2d, conv3d
 
-from einconv.einconvnd import einconv1d, einconv2d
+from einconv.einconvnd import einconv1d, einconv2d, einconv3d
 
 
 @mark.parametrize("device", DEVICES, ids=DEVICE_IDS)
@@ -58,5 +60,27 @@ def test_einconv2d(case: Dict, device: device):
 
     conv_output = conv2d(x, weight, bias=bias, **case["conv_kwargs"])
     einconv_output = einconv2d(x, weight, bias=bias, **case["conv_kwargs"])
+
+    report_nonclose(conv_output, einconv_output)
+
+
+@mark.parametrize("device", DEVICES, ids=DEVICE_IDS)
+@mark.parametrize("case", CONV_3D_FUNCTIONAL_CASES, ids=CONV_3D_FUNCTIONAL_IDS)
+def test_einconv3d(case: Dict, device: device):
+    """Compare PyTorch's conv3d with einconv's einconv3d.
+
+    Args:
+        case: Dictionary describing the test case.
+        device: Device for executing the test.
+    """
+    manual_seed(case["seed"])
+    x = case["input_fn"]().to(device)
+    weight = case["weight_fn"]().to(device)
+    bias = case["bias_fn"]()
+    if bias is not None:
+        bias = bias.to(device)
+
+    conv_output = conv3d(x, weight, bias=bias, **case["conv_kwargs"])
+    einconv_output = einconv3d(x, weight, bias=bias, **case["conv_kwargs"])
 
     report_nonclose(conv_output, einconv_output)
