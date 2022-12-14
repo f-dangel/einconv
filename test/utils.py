fa738bfc4677833cd006530e17e31f67fe9bd728
Felix Dangel <48687646+f-dangel@users.noreply.github.com>
2022-12-14 14:43:47 +0100

    [ADD] Converter `torch.nn.Conv{1,2,3}d` to `EinconvNd` (#7)
    * [ADD] Converter `torch.nn.Conv{1,2,3}d` to `EinconvNd`
    
    * [FIX] Remove unused imports

--- test/utils.py
+++ test/utils.py
@@ -1,10 +1,9 @@
 """Utility functions for testing."""
 
 from types import LambdaType
-from typing import Any, Dict, List
+from typing import Dict, List
 
 from torch import Tensor, allclose, cuda, device, isclose
-from torch.nn import Module
 
 
 def report_nonclose(
@@ -86,49 +85,3 @@ def make_id(case: Dict) -> str:
             parts.append("_".join([key_str, value_str]))
 
     return "_".join(parts)
-
-
-def compare_attributes(obj1: Any, obj2: Any, attributes: List[str]):
-    """Compare multiple attributes of two objects.
-
-    Args:
-        obj1: First object.
-        obj2: Second object.
-        attributes: Attribute names to compare.
-
-    Raises:
-        ValueError: If two attributes don't match.
-    """
-    for attr in attributes:
-        attr1 = getattr(obj1, attr)
-        attr2 = getattr(obj2, attr)
-        if attr1 != attr2:
-            raise ValueError(f"'{attr}' attribute does not match: {attr1} ≠ {attr2}")
-
-
-def sync_parameters(module1: Module, module2: Module) -> None:
-    """Copy the values of ``module1``'s parameters to ``module2``'s parameters.
-
-    Args:
-        module1: Module whose parameters will be used for syncing.
-        module2: Module whose parameters will be synced to those of ``module1``.
-
-    Raises:
-        ValueError: If module parameter names don't match.
-    """
-    param_names1 = {n for (n, _) in module1.named_parameters()}
-    param_names2 = {n for (n, _) in module2.named_parameters()}
-
-    if param_names1 != param_names2:
-        raise ValueError(
-            f"Module parameters have different names: {param_names1} ≠ {param_names2}"
-        )
-
-    for param_name in param_names1:
-        param1 = getattr(module1, param_name)
-        param2 = getattr(module2, param_name)
-
-        if param1 is not None or param2 is not None:
-            attributes = ["shape", "dtype", "device"]
-            compare_attributes(param1, param2, attributes)
-            param2.data = param1.data
