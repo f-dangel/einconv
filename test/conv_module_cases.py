42f4e59360dc2ecf7da098dc6a819760db51d255
Felix Dangel <fdangel@tue.mpg.de>
2022-12-14 11:08:36 +0100

    [REF] Introduce test utilities to create modules and sync parameters

--- test/conv_module_cases.py
+++ test/conv_module_cases.py
@@ -1,8 +1,13 @@
 """Contains test cases for ``einconv``'s module implementation of convolution."""
 
 from test.utils import make_id
+from typing import Dict, Union
 
+import torch
 from torch import rand
+from torch.nn import Conv1d, Conv2d, Conv3d
+
+from einconv.einconvnd import EinconvNd
 
 CONV_1D_MODULE_CASES = [
     # no kwargs except for bias disabled
@@ -57,3 +62,51 @@ CONV_1D_MODULE_CASES = [
 ]
 
 CONV_1D_MODULE_IDS = [make_id(case) for case in CONV_1D_MODULE_CASES]
+
+
+def conv_module_from_case(
+    N: int, case: Dict, device: torch.device, dtype: Union[torch.dtype, None] = None
+) -> Union[Conv1d, Conv2d, Conv3d]:
+    """Create PyTorch convolution module from a case.
+
+    Args:
+        N: Convolution dimension.
+        case: Dictionary describing the module.
+        dtype: Data type assumed by the layer. Default: ``None`` (``torch.float32``).
+
+    Returns:
+        PyTorch convolution module.
+    """
+    conv_cls = {1: Conv1d, 2: Conv2d, 3: Conv3d}[N]
+    return conv_cls(
+        case["in_channels"],
+        case["out_channels"],
+        case["kernel_size"],
+        **case["conv_kwargs"],
+        device=device,
+        dtype=dtype
+    )
+
+
+def einconv_module_from_case(
+    N: int, case: Dict, device: torch.device, dtype: Union[torch.dtype, None] = None
+) -> EinconvNd:
+    """Create einconv convolution module from a case.
+
+    Args:
+        N: Convolution dimension.
+        case: Dictionary describing the module.
+        dtype: Data type assumed by the layer. Default: ``None`` (``torch.float32``).
+
+    Returns:
+        einconv convolution module.
+    """
+    return EinconvNd(
+        N,
+        case["in_channels"],
+        case["out_channels"],
+        case["kernel_size"],
+        **case["conv_kwargs"],
+        device=device,
+        dtype=dtype
+    )
