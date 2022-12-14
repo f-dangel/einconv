88f844f1bc7e1eb39ceaf8d9861e6501798e3568
Felix Dangel <fdangel@tue.mpg.de>
2022-12-14 11:24:36 +0100

    [TEST] Add EinconvNd layer tests for `N=2,3`

--- test/conv_module_cases.py
+++ test/conv_module_cases.py
@@ -26,12 +26,10 @@ CONV_1D_MODULE_CASES = [
     # non-default kwargs
     {
         "seed": 0,
-        # (batch_size, in_channels, num_pixels)
         "input_fn": lambda: rand(2, 3, 50),
         "in_channels": 3,
         "out_channels": 9,
         "kernel_size": 5,
-        # stride, padding, dilation, groups, padding_mode, bias
         "conv_kwargs": {
             "stride": 2,
             "padding": 1,
@@ -44,12 +42,10 @@ CONV_1D_MODULE_CASES = [
     # non-default kwargs as tuples
     {
         "seed": 0,
-        # (batch_size, in_channels, num_pixels)
         "input_fn": lambda: rand(2, 6, 99),
         "in_channels": 6,
         "out_channels": 9,
         "kernel_size": (5,),
-        # stride, padding, dilation, groups, padding_mode, bias
         "conv_kwargs": {
             "stride": (2,),
             "padding": (1,),
@@ -62,6 +58,105 @@ CONV_1D_MODULE_CASES = [
 ]
 
 CONV_1D_MODULE_IDS = [make_id(case) for case in CONV_1D_MODULE_CASES]
+
+
+CONV_2D_MODULE_CASES = [
+    # no kwargs except for bias disabled
+    {
+        "seed": 0,
+        # (batch_size, in_channels, num_pixels_h, num_pixels_w)
+        "input_fn": lambda: rand(2, 3, 50, 40),
+        "in_channels": 3,
+        "out_channels": 4,
+        "kernel_size": 5,
+        # stride, padding, dilation, groups, padding_mode, bias
+        "conv_kwargs": {
+            "bias": False,
+        },
+    },
+    # non-default kwargs
+    {
+        "seed": 0,
+        "input_fn": lambda: rand(2, 3, 50, 45),
+        "in_channels": 3,
+        "out_channels": 9,
+        "kernel_size": 5,
+        "conv_kwargs": {
+            "stride": 2,
+            "padding": 1,
+            "dilation": 2,
+            "groups": 3,
+            "padding_mode": "zeros",
+            "bias": True,
+        },
+    },
+    # non-default kwargs as tuples
+    {
+        "seed": 0,
+        "input_fn": lambda: rand(2, 6, 99, 88),
+        "in_channels": 6,
+        "out_channels": 9,
+        "kernel_size": (5, 4),
+        "conv_kwargs": {
+            "stride": (2, 3),
+            "padding": (1, 0),
+            "dilation": (2, 1),
+            "groups": 3,
+            "padding_mode": "zeros",
+            "bias": True,
+        },
+    },
+]
+CONV_2D_MODULE_IDS = [make_id(case) for case in CONV_2D_MODULE_CASES]
+
+CONV_3D_MODULE_CASES = [
+    # no kwargs except for bias disabled
+    {
+        "seed": 0,
+        # (batch_size, in_channels, num_pixels_h, num_pixels_w)
+        "input_fn": lambda: rand(2, 3, 20, 15, 10),
+        "in_channels": 3,
+        "out_channels": 4,
+        "kernel_size": 5,
+        # stride, padding, dilation, groups, padding_mode, bias
+        "conv_kwargs": {
+            "bias": False,
+        },
+    },
+    # non-default kwargs
+    {
+        "seed": 0,
+        "input_fn": lambda: rand(2, 3, 50, 45, 40),
+        "in_channels": 3,
+        "out_channels": 9,
+        "kernel_size": 5,
+        "conv_kwargs": {
+            "stride": 2,
+            "padding": 1,
+            "dilation": 2,
+            "groups": 3,
+            "padding_mode": "zeros",
+            "bias": True,
+        },
+    },
+    # non-default kwargs as tuples
+    {
+        "seed": 0,
+        "input_fn": lambda: rand(2, 6, 99, 88, 77),
+        "in_channels": 6,
+        "out_channels": 9,
+        "kernel_size": (5, 4, 3),
+        "conv_kwargs": {
+            "stride": (2, 3, 2),
+            "padding": (1, 0, 2),
+            "dilation": (2, 1, 3),
+            "groups": 3,
+            "padding_mode": "zeros",
+            "bias": True,
+        },
+    },
+]
+CONV_3D_MODULE_IDS = [make_id(case) for case in CONV_3D_MODULE_CASES]
 
 
 def conv_module_from_case(
--- test/test_einconvnd_module.py
+++ test/test_einconvnd_module.py
@@ -3,6 +3,10 @@
 from test.conv_module_cases import (
     CONV_1D_MODULE_CASES,
     CONV_1D_MODULE_IDS,
+    CONV_2D_MODULE_CASES,
+    CONV_2D_MODULE_IDS,
+    CONV_3D_MODULE_CASES,
+    CONV_3D_MODULE_IDS,
     conv_module_from_case,
     einconv_module_from_case,
 )
@@ -12,6 +16,29 @@ from typing import Dict, Union
 import torch
 from pytest import mark
 from torch import device, manual_seed
+
+
+def _test_EinconvNd(
+    N: int, case: Dict, device: device, dtype: Union[torch.dtype, None] = None
+):
+    """Compare PyTorch's ConvNd layer with einconv's EinconvNd layer.
+
+    Args:
+        N: Convolution dimension.
+        case: Dictionary describing the test case.
+        device: Device for executing the test.
+        dtype: Data type assumed by the layer. Default: ``None`` (``torch.float32``).
+    """
+    manual_seed(case["seed"])
+    x = case["input_fn"]().to(device)
+
+    conv_module = conv_module_from_case(N, case, device, dtype=dtype)
+    einconv_module = einconv_module_from_case(N, case, device, dtype=dtype)
+
+    # use same parameters
+    sync_parameters(conv_module, einconv_module)
+
+    report_nonclose(conv_module(x), einconv_module(x), atol=1e-6, rtol=1e-5)
 
 
 @mark.parametrize("device", DEVICES, ids=DEVICE_IDS)
@@ -24,14 +51,33 @@ def test_Einconv1d(case: Dict, device: device, dtype: Union[torch.dtype, None] =
         device: Device for executing the test.
         dtype: Data type assumed by the layer. Default: ``None`` (``torch.float32``).
     """
-    manual_seed(case["seed"])
-    x = case["input_fn"]().to(device)
-
     N = 1
-    conv_module = conv_module_from_case(N, case, device, dtype=dtype)
-    einconv_module = einconv_module_from_case(N, case, device, dtype=dtype)
+    _test_EinconvNd(N, case, device, dtype=dtype)
 
-    # use same parameters
-    sync_parameters(conv_module, einconv_module)
 
-    report_nonclose(conv_module(x), einconv_module(x), atol=1e-7)
+@mark.parametrize("device", DEVICES, ids=DEVICE_IDS)
+@mark.parametrize("case", CONV_2D_MODULE_CASES, ids=CONV_2D_MODULE_IDS)
+def test_Einconv2d(case: Dict, device: device, dtype: Union[torch.dtype, None] = None):
+    """Compare PyTorch's Conv2d layer with einconv's EinconvNd layer.
+
+    Args:
+        case: Dictionary describing the test case.
+        device: Device for executing the test.
+        dtype: Data type assumed by the layer. Default: ``None`` (``torch.float32``).
+    """
+    N = 2
+    _test_EinconvNd(N, case, device, dtype=dtype)
+
+
+@mark.parametrize("device", DEVICES, ids=DEVICE_IDS)
+@mark.parametrize("case", CONV_3D_MODULE_CASES, ids=CONV_3D_MODULE_IDS)
+def test_Einconv3d(case: Dict, device: device, dtype: Union[torch.dtype, None] = None):
+    """Compare PyTorch's Conv3d layer with einconv's EinconvNd layer.
+
+    Args:
+        case: Dictionary describing the test case.
+        device: Device for executing the test.
+        dtype: Data type assumed by the layer. Default: ``None`` (``torch.float32``).
+    """
+    N = 3
+    _test_EinconvNd(N, case, device, dtype=dtype)
