1f6fcab56552ae1adf091a68f5f7b56b001416b7
Felix Dangel <fdangel@tue.mpg.de>
2023-02-19 16:48:32 -0500

    [ADD] N-dimensional generalization of `torch.nn.functional.unfold`

--- einconv/__init__.py
+++ einconv/__init__.py
@@ -1,1 +1,7 @@
 """einconv library."""
+
+from einconv.unfoldnd import unfoldNd
+
+__all__ = [
+    "unfoldNd",
+]
---
+++ test/test_unfoldNd.py
@@ -0,0 +1,45 @@
+"""Contains tests for ``einconv/unfoldnd.py"""
+
+from test.unfold_cases import (
+    UNFOLD_PROBLEMS_1D,
+    UNFOLD_PROBLEMS_1D_IDS,
+    UNFOLD_PROBLEMS_2D,
+    UNFOLD_PROBLEMS_2D_IDS,
+    UNFOLD_PROBLEMS_3D,
+    UNFOLD_PROBLEMS_3D_IDS,
+)
+from test.utils import DEVICE_IDS, DEVICES, report_nonclose
+from typing import Dict
+
+import torch
+import unfoldNd
+from pytest import mark
+
+import einconv
+
+
+@mark.parametrize(
+    "case",
+    UNFOLD_PROBLEMS_1D + UNFOLD_PROBLEMS_2D + UNFOLD_PROBLEMS_3D,
+    ids=UNFOLD_PROBLEMS_1D_IDS + UNFOLD_PROBLEMS_2D_IDS + UNFOLD_PROBLEMS_3D_IDS,
+)
+@mark.parametrize("device", DEVICES, ids=DEVICE_IDS)
+def test_unfoldNd(case: Dict, device: torch.device):
+    """Compare ``einconv.unfoldnd.unfoldNd`` with ``unfoldNd`` package.
+
+    Args:
+        case: Dictionary describing the test case.
+        device: Device to execute the test on.
+    """
+    seed = case["seed"]
+    input_fn = case["input_fn"]
+    kernel_size = case["kernel_size"]
+    unfold_kwargs = case["unfold_kwargs"]
+
+    torch.manual_seed(seed)
+    inputs = input_fn().to(device)
+
+    result_unfold = unfoldNd.unfoldNd(inputs, kernel_size, **unfold_kwargs)
+    result_einconv = einconv.unfoldNd(inputs, kernel_size, **unfold_kwargs)
+
+    report_nonclose(result_unfold, result_einconv)
