452a5d4347a070220ed3b444d8e5d5ebd71dbcd7
Felix Dangel <felix.dangel@vectorinstitute.ai>
2023-06-12 22:23:23 -0400

    [FIX] Re-introduce missing variable

--- test/kfc_factor_cases.py
+++ test/kfc_factor_cases.py
@@ -18,7 +18,6 @@ for case in CONV_1D_MODULE_CASES:
     case_copy = {**case}
     case_copy["kfc_factor_kwargs"] = case_copy.pop("conv_kwargs")
     KFC_FACTOR_1D_CASES.append(case_copy)
-
 KFC_FACTOR_1D_IDS = [make_id(case) for case in KFC_FACTOR_1D_CASES]
 
 KFC_FACTOR_2D_CASES = []
@@ -31,6 +30,7 @@ for case in CONV_2D_MODULE_CASES:
     case_copy = {**case}
     case_copy["kfc_factor_kwargs"] = case_copy.pop("conv_kwargs")
     KFC_FACTOR_2D_CASES.append(case_copy)
+KFC_FACTOR_2D_IDS = [make_id(case) for case in KFC_FACTOR_2D_CASES]
 
 KFC_FACTOR_3D_CASES = []
 for case in UNFOLD_3D_CASES:
@@ -42,5 +42,4 @@ for case in CONV_3D_MODULE_CASES:
     case_copy = {**case}
     case_copy["kfc_factor_kwargs"] = case_copy.pop("conv_kwargs")
     KFC_FACTOR_3D_CASES.append(case_copy)
-
 KFC_FACTOR_3D_IDS = [make_id(case) for case in KFC_FACTOR_3D_CASES]
