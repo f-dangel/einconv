51d6dd26a8a25168c1a806ff7413351675336ba0
Felix Dangel <felix.dangel@vectorinstitute.ai>
2023-07-02 17:51:41 -0400

    [DOC] Polish KFC docstrings

--- einconv/expressions/convNd_kfc.py
+++ einconv/expressions/convNd_kfc.py
@@ -1,6 +1,6 @@
-"""Implementation of the input-based of the KFC Fisher approximation for convolutions.
+"""Input-based factor of the KFC Fisher approximation for convolutions.
 
-Details see:
+KFC was introduced by:
 
 - Grosse, R., & Martens, J. (2016). A Kronecker-factored approximate Fisher matrix
   for convolution layers. International Conference on Machine Learning (ICML).
@@ -23,12 +23,101 @@ def einsum_expression(
     dilation: Union[int, Tuple[int, ...]] = 1,
     groups: int = 1,
 ) -> Tuple[str, List[Tensor], Tuple[int, ...]]:
+    """Generate einsum expression of input-based KFC factor for convolution.
+
+    Args:
+        x: Convolution input. Has shape ``[batch_size, in_channels, *input_sizes]``
+            where ``len(input_sizes) == N``.
+        kernel_size: Kernel dimensions. Can be a single integer (shared along all
+            spatial dimensions), or an ``N``-tuple of integers.
+        stride: Stride of the convolution. Can be a single integer (shared along all
+            spatial dimensions), or an ``N``-tuple of integers. Default: ``1``.
+        padding: Padding of the convolution. Can be a single integer (shared along
+            all spatial dimensions), an ``N``-tuple of integers, or a string.
+            Default: ``0``. Allowed strings are ``'same'`` and ``'valid'``.
+        dilation: Dilation of the convolution. Can be a single integer (shared along
+            all spatial dimensions), or an ``N``-tuple of integers. Default: ``1``.
+        groups: In how many groups to split the input channels. Default: ``1``.
+
+    Returns:
+        Einsum equation
+        Einsum operands in order un-grouped input, patterns, un-grouped input, \
+        patterns, normalization scaling
+        Output shape: ``[groups, in_channels //groups * tot_kernel_sizes,\
+        in_channels //groups * tot_kernel_sizes]``
+    """
     N = x.dim() - 2
     equation = _equation(N)
     operands, shape = _operands_and_shape(
         x, kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups
     )
     return equation, operands, shape
+
+
+def _operands_and_shape(
+    x: Tensor,
+    kernel_size: Union[int, Tuple[int, ...]],
+    stride: Union[int, Tuple[int, ...]] = 1,
+    padding: Union[int, str, Tuple[int, ...]] = 0,
+    dilation: Union[int, Tuple[int, ...]] = 1,
+    groups: int = 1,
+) -> Tuple[List[Tensor], Tuple[int, ...]]:
+    """Generate einsum operands for KFC factor.
+
+    Args:
+        x: Convolution input. Has shape ``[batch_size, in_channels, *input_sizes]``
+            where ``len(input_sizes) == N``.
+        kernel_size: Kernel dimensions. Can be a single integer (shared along all
+            spatial dimensions), or an ``N``-tuple of integers.
+        stride: Stride of the convolution. Can be a single integer (shared along all
+            spatial dimensions), or an ``N``-tuple of integers. Default: ``1``.
+        padding: Padding of the convolution. Can be a single integer (shared along
+            all spatial dimensions), an ``N``-tuple of integers, or a string.
+            Default: ``0``. Allowed strings are ``'same'`` and ``'valid'``.
+        dilation: Dilation of the convolution. Can be a single integer (shared along
+            all spatial dimensions), or an ``N``-tuple of integers. Default: ``1``.
+        groups: In how many groups to split the input channels. Default: ``1``.
+
+    Returns:
+        Tensor list containing the operands. Convention: Input, followed by
+        index pattern tensors, followed by index pattern tensors, followed by x.
+    """
+    # convert into tuple format
+    N = x.dim() - 2
+    input_sizes = x.shape[2:]
+    t_kernel_size: Tuple[int, ...] = _tuple(kernel_size, N)
+    t_dilation: Tuple[int, ...] = _tuple(dilation, N)
+    t_padding: Union[Tuple[int, ...], str] = (
+        padding if isinstance(padding, str) else _tuple(padding, N)
+    )
+    t_stride: Tuple[int, ...] = _tuple(stride, N)
+
+    patterns: List[Tensor] = [
+        index_pattern(
+            input_sizes[n],
+            t_kernel_size[n],
+            stride=t_stride[n],
+            padding=t_padding if isinstance(t_padding, str) else t_padding[n],
+            dilation=t_dilation[n],
+            device=x.device,
+            dtype=x.dtype,
+        )
+        for n in range(N)
+    ]
+    x_ungrouped = rearrange(x, "n (g c_in) ... -> n g c_in ...", g=groups)
+    batch_size = x.shape[0]
+    scale = Tensor([1.0 / batch_size]).to(x.device).to(x.dtype)
+    operands = [x_ungrouped, *patterns, *patterns, x_ungrouped, scale]
+
+    kernel_tot_sizes = int(Tensor(t_kernel_size).int().prod())
+    in_channels = x.shape[1]
+    shape = (
+        groups,
+        in_channels // groups * kernel_tot_sizes,
+        in_channels // groups * kernel_tot_sizes,
+    )
+
+    return operands, shape
 
 
 def _equation(N: int) -> str:
@@ -99,54 +188,3 @@ def _equation(N: int) -> str:
     )
 
     return "->".join([input_equation, output_str])
-
-
-def _operands_and_shape(
-    x: Tensor,
-    kernel_size: Union[int, Tuple[int, ...]],
-    stride: Union[int, Tuple[int, ...]] = 1,
-    padding: Union[int, str, Tuple[int, ...]] = 0,
-    dilation: Union[int, Tuple[int, ...]] = 1,
-    groups: int = 1,
-) -> List[Tensor]:
-    """Generate einsum operands for KFC factor.
-
-    Returns:
-        Tensor list containing the operands. Convention: Input, followed by
-        index pattern tensors, followed by index pattern tensors, followed by x.
-    """
-    N = x.dim() - 2
-    (batch_size, in_channels), input_sizes = x.shape[:2], x.shape[2:]
-
-    # convert into tuple format
-    t_kernel_size: Tuple[int, ...] = _tuple(kernel_size, N)
-    t_dilation: Tuple[int, ...] = _tuple(dilation, N)
-    t_padding: Union[Tuple[int, ...], str] = (
-        padding if isinstance(padding, str) else _tuple(padding, N)
-    )
-    t_stride: Tuple[int, ...] = _tuple(stride, N)
-
-    patterns: List[Tensor] = [
-        index_pattern(
-            input_sizes[n],
-            t_kernel_size[n],
-            stride=t_stride[n],
-            padding=t_padding if isinstance(t_padding, str) else t_padding[n],
-            dilation=t_dilation[n],
-            device=x.device,
-            dtype=x.dtype,
-        )
-        for n in range(N)
-    ]
-    x_ungrouped = rearrange(x, "n (g c_in) ... -> n g c_in ...", g=groups)
-    scale = Tensor([1.0 / batch_size]).to(x.device).to(x.dtype)
-
-    kernel_tot_size = Tensor(t_kernel_size).int().prod()
-
-    output_shape = (
-        groups,
-        in_channels // groups * kernel_tot_size,
-        in_channels // groups * kernel_tot_size,
-    )
-
-    return [x_ungrouped, *patterns, *patterns, x_ungrouped, scale], output_shape
