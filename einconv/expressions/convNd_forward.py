3f37b13f2f67f816be5cae0e49ac7dd8dc584f93
Felix Dangel <48687646+f-dangel@users.noreply.github.com>
2023-07-03 11:36:20 -0400

    [REF] Inline equation/operands/shape creation (#27)
    Co-authored-by: Felix Dangel <felix.dangel@vectorinstitute.ai>

--- einconv/expressions/convNd_forward.py
+++ einconv/expressions/convNd_forward.py
@@ -40,50 +40,23 @@ def einsum_expression(
         Output shape: ``[batch_size, out_channels, *output_sizes]``.
     """
     N = x.dim() - 2
-    equation = _equation(N)
-    operands, shape = _operands_and_shape(
-        x, weight, stride=stride, padding=padding, dilation=dilation, groups=groups
-    )
-    return equation, operands, shape
 
+    # construct einsum equation
+    x_str = "n g c_in " + " ".join([f"i{i}" for i in range(N)])
+    pattern_strs: List[str] = [f"k{i} o{i} i{i}" for i in range(N)]
+    weight_str = "g c_out c_in " + " ".join([f"k{i}" for i in range(N)])
+    lhs = ",".join([x_str, *pattern_strs, weight_str])
 
-def _operands_and_shape(
-    x: Tensor,
-    weight: Tensor,
-    stride: Union[int, Tuple[int, ...]] = 1,
-    padding: Union[int, str, Tuple[int, ...]] = 0,
-    dilation: Union[int, Tuple[int, ...]] = 1,
-    groups: int = 1,
-) -> Tuple[List[Union[Tensor, Parameter]], Tuple[int, ...]]:
-    """Prepare operands for contraction with einsum.
+    rhs = "n g c_out " + " ".join([f"o{i}" for i in range(N)])
 
-    Args:
-        x: Convolution input. Has shape ``[batch_size, in_channels, *input_sizes]``
-            where ``len(input_sizes) == N``.
-        weight: Kernel of the convolution. Has shape ``[out_channels,
-            in_channels / groups, *kernel_size]`` where ``kernel_size`` is an
-            ``N``-tuple of kernel dimensions.
-        stride: Stride of the convolution. Can be a single integer (shared along all
-            spatial dimensions), or an ``N``-tuple of integers. Default: ``1``.
-        padding: Padding of the convolution. Can be a single integer (shared along
-            all spatial dimensions), an ``N``-tuple of integers, or a string.
-            Default: ``0``. Allowed strings are ``'same'`` and ``'valid'``.
-        dilation: Dilation of the convolution. Can be a single integer (shared along
-            all spatial dimensions), or an ``N``-tuple of integers. Default: ``1``.
-        groups: In how many groups to split the input channels. Default: ``1``.
+    equation = "->".join([lhs, rhs])
+    equation = translate_to_torch(equation)
 
-    Returns:
-        Tensor list containing the operands in order un-grouped input, index patterns, \
-        un-grouped weight.
-        Output shape.
-    """
-    N = x.dim() - 2
-    input_size = x.shape[2:]
-    kernel_size = weight.shape[2:]
+    # construct einsum operands
     patterns = create_conv_index_patterns(
         N,
-        input_size,
-        kernel_size,
+        input_size=x.shape[2:],
+        kernel_size=weight.shape[2:],
         stride=stride,
         padding=padding,
         dilation=dilation,
@@ -94,30 +67,10 @@ def _operands_and_shape(
     weight_ungrouped = rearrange(weight, "(g c_out) ... -> g c_out ...", g=groups)
     operands = [x_ungrouped, *patterns, weight_ungrouped]
 
-    output_sizes = [p.shape[1] for p in patterns]
+    # construct output shape
+    output_size = [p.shape[1] for p in patterns]
     batch_size = x.shape[0]
     out_channels = weight.shape[0]
-    shape = (batch_size, out_channels, *output_sizes)
+    shape = (batch_size, out_channels, *output_size)
 
-    return operands, shape
-
-
-def _equation(N: int) -> str:
-    """Generate einsum equation for convolution.
-
-    Args:
-        N: Convolution dimension.
-
-    Returns:
-        Einsum equation for N-dimensional convolution. Operand order is un-grouped \
-        input, patterns, un-grouped weight.
-    """
-    x_str = "n g c_in " + " ".join([f"i{i}" for i in range(N)])
-    pattern_strs: List[str] = [f"k{i} o{i} i{i}" for i in range(N)]
-    weight_str = "g c_out c_in " + " ".join([f"k{i}" for i in range(N)])
-    lhs = ",".join([x_str, *pattern_strs, weight_str])
-
-    rhs = "n g c_out " + " ".join([f"o{i}" for i in range(N)])
-
-    equation = "->".join([lhs, rhs])
-    return translate_to_torch(equation)
+    return equation, operands, shape
--- einconv/expressions/convNd_unfold.py
+++ einconv/expressions/convNd_unfold.py
@@ -31,76 +31,11 @@ def einsum_expression(
     Returns:
         Einsum equation
         Einsum operands in order input, patterns
-        Output shape: ``[batch_size, in_channels, tot_output_sizes]``
+        Output shape: ``[batch_size, in_channels, tot_output_size]``
     """
     N = x.dim() - 2
-    equation = _equation(N)
-    operands, shape = _operands_and_shape(
-        x, kernel_size, padding=padding, stride=stride, dilation=dilation
-    )
-    return equation, operands, shape
 
-
-def _operands_and_shape(
-    x: Tensor,
-    kernel_size: Union[int, Tuple[int, ...]],
-    stride: Union[int, Tuple[int, ...]] = 1,
-    padding: Union[int, Tuple[int, ...], str] = 0,
-    dilation: Union[int, Tuple[int, ...]] = 1,
-) -> Tuple[List[Tensor], Tuple[int, ...]]:
-    """Prepare operands for contraction with einsum.
-
-    Args:
-        x: Convolution input. Has shape ``[batch_size, in_channels, *input_sizes]``
-            where ``len(input_sizes) == N``.
-        kernel_size: Kernel dimensions. Can be a single integer (shared along all
-            spatial dimensions), or an ``N``-tuple of integers.
-        stride: Stride of the convolution. Can be a single integer (shared along all
-            spatial dimensions), or an ``N``-tuple of integers. Default: ``1``.
-        padding: Padding of the convolution. Can be a single integer (shared along
-            all spatial dimensions), an ``N``-tuple of integers, or a string.
-            Default: ``0``. Allowed strings are ``'same'`` and ``'valid'``.
-        dilation: Dilation of the convolution. Can be a single integer (shared along
-            all spatial dimensions), or an ``N``-tuple of integers. Default: ``1``.
-
-    Returns:
-        Tensor list containing the operands in order input, patterns.
-        Output shape.
-    """
-    N = x.dim() - 2
-    input_size = x.shape[2:]
-    patterns = create_conv_index_patterns(
-        N,
-        input_size,
-        kernel_size,
-        stride=stride,
-        padding=padding,
-        dilation=dilation,
-        device=x.device,
-        dtype=x.dtype,
-    )
-    operands = [x, *patterns]
-
-    output_tot_size = int(Tensor([p.shape[1] for p in patterns]).int().prod())
-    t_kernel_size = _tuple(kernel_size, N)
-    kernel_tot_size = int(Tensor(t_kernel_size).int().prod())
-    batch_size, in_channels = x.shape[:2]
-    shape = (batch_size, in_channels * kernel_tot_size, output_tot_size)
-
-    return operands, shape
-
-
-def _equation(N: int) -> str:
-    """Generate einsum equation for unfold.
-
-    The arguments are ``input, *index_patterns -> output``.
-
-    Args:
-        N: Convolution dimension.
-
-    Returns:
-        Einsum equation for N-dimensional convolution.
-    """
+    # construct einsum equation
     x_str = "n c_in " + " ".join([f"i{i}" for i in range(N)])
     pattern_strs: List[str] = [f"k{i} o{i} i{i}" for i in range(N)]
     lhs = ",".join([x_str, *pattern_strs])
@@ -113,4 +48,26 @@ def _equation(N: int) -> str:
     )
 
     equation = "->".join([lhs, rhs])
-    return translate_to_torch(equation)
+    equation = translate_to_torch(equation)
+
+    # construct einsum operands
+    patterns = create_conv_index_patterns(
+        N,
+        input_size=x.shape[2:],
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        dilation=dilation,
+        device=x.device,
+        dtype=x.dtype,
+    )
+    operands = [x, *patterns]
+
+    # construct output shape
+    output_tot_size = int(Tensor([p.shape[1] for p in patterns]).int().prod())
+    t_kernel_size = _tuple(kernel_size, N)
+    kernel_tot_size = int(Tensor(t_kernel_size).int().prod())
+    batch_size, in_channels = x.shape[:2]
+    shape = (batch_size, in_channels * kernel_tot_size, output_tot_size)
+
+    return equation, operands, shape
